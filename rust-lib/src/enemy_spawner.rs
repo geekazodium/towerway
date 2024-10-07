a2dbf24c0edf7255f43f05dc56f459b606a95d4f
geekazodium <59382423+geekazodium@users.noreply.github.com>
2024-10-07 16:20:01 +0800

    Tweaked enemy spawning behavior.
    now's the part where we need to enter all the data into this and heck
    aaaaaaaaa

--- rust-lib/src/enemy_spawner.rs
+++ rust-lib/src/enemy_spawner.rs
@@ -1,4 +1,4 @@
-use godot::{classes::{INode, Node, PackedScene, Path2D}, obj::{Base, Gd, WithBaseField}, prelude::{godot_api, GodotClass}};
+use godot::{builtin::{Array, Callable}, classes::{Area2D, IArea2D, INode, Node, PackedScene, Path2D, TextureProgressBar}, obj::{Base, Gd, WithBaseField}, prelude::{godot_api, GodotClass}};
 
 use crate::{ingame_state_tracker::{GameplayState, IngameStateTracker}, player_health::PlayerHealth};
 
@@ -12,8 +12,11 @@ pub struct EnemySpawner{
     timer: f64,
     #[export]
     enemies: Option<Gd<PackedScene>>,
-    #[export]
     enabled: bool,
+    #[export]
+    wait_for: Option<Gd<EnemySpawner>>,
+    #[export]
+    spawns_left: u32,
     #[export]
     game_state: Option<Gd<IngameStateTracker>>
 }
@@ -24,12 +27,23 @@ impl INode for EnemySpawner{
         if self.game_state.is_none(){
             self.game_state = Some(self.base().get_parent().unwrap().get_parent().unwrap().find_child("IngameStateTracker".into()).unwrap().cast());
         }
+        let waitfor = self.get_wait_for();
+        if waitfor.is_none(){
+            self.enabled = true;
+        }else{
+            self.enabled = false;
+            let mut c = waitfor.unwrap();
+            c.connect("spawning_end".into(),Callable::from_object_method(&self.to_gd(),"on_spawning_end"));
+        }
     }
     fn physics_process(&mut self, delta: f64){
         if self.get_game_state().unwrap().bind().get_state() != GameplayState::DEFENDING{
             return;
         }
-        if !self.get_enabled(){
+        if !self.enabled{
+            return;
+        }
+        if self.is_done(){
             return;
         }
         self.timer += delta;
@@ -38,7 +52,27 @@ impl INode for EnemySpawner{
             let instance = self.enemies.clone().expect("enemy scene not set").instantiate().unwrap();
             let mut parent = self.base().get_parent().unwrap();
             parent.add_child(instance);
+            self.spawns_left -= 1;
+            if self.is_done(){
+                self.base_mut().emit_signal("spawning_end".into(), &[]);
+            }
         }
+    }
+}
+
+#[godot_api]
+impl EnemySpawner{
+    #[signal]
+    fn spawning_end();
+    #[func]
+    fn on_spawning_end(&mut self){
+        self.enabled = true;
+    }
+}
+
+impl EnemySpawner{
+    fn is_done(&self) -> bool{
+        self.spawns_left <= 0
     }
 }
 
@@ -53,5 +87,39 @@ pub struct EnemyPath{
 impl EnemyPath{
     pub fn hit_player(&self){
         self.get_player_health().unwrap().bind_mut().take_damage(1);
+    }
+}
+
+#[derive(GodotClass)]
+#[class(base = Area2D, init)]
+struct EnemySpawnerProgressTracker{
+    #[export]
+    progress_bar: Option<Gd<TextureProgressBar>>,
+    #[export]
+    spawners: Array<Gd<EnemySpawner>>,
+    base: Base<Area2D>,
+    #[export]
+    gamestate: Option<Gd<IngameStateTracker>>
+}
+
+#[godot_api]
+impl IArea2D for EnemySpawnerProgressTracker{
+    fn ready(&mut self){
+        self.get_progress_bar().unwrap().set_max(self.spawners.len() as f64);
+    }
+    fn physics_process(&mut self, _delta: f64) {
+        
+        let mut active_spawners_count = self.spawners.len();
+        for spawner in self.spawners.iter_shared() {
+            if spawner.bind().is_done(){
+                active_spawners_count -= 1;
+            }
+        }
+
+        self.get_progress_bar().unwrap().set_value(active_spawners_count as f64);
+
+        if active_spawners_count == 0 && self.base().get_overlapping_areas().len() == 0{
+            self.get_gamestate().unwrap().bind_mut().win();
+        }
     }
 }
